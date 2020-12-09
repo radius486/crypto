a7ab789e9346775915cbd3fc79eb4acaddb6121e
Radziyon Hryshayeu <rhryshayeu@allot.com>
2020-12-09 21:16:08 +0300

    Get data and render pairs.

--- src/components/currency-pair.vue
+++ src/components/currency-pair.vue
@@ -1,7 +1,19 @@
 <template>
   <div class='currency-pair'>
-    {{ pair }}
-    {{ index }}
+    <table>
+      <tr>
+        <td>{{ pair.Label }}</td>
+        <td></td>
+        <td>{{ pair.Price }}</td>
+        <td>Купить</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>Продать</td>
+      </tr>
+    </table>
     <button @click.prevent='deletePair'>Удалить</button>
   </div>
 </template>
@@ -11,7 +23,7 @@ export default {
   name: 'CurrencyPair',
   props: {
     pair: {
-      type: String,
+      type: Object,
       required: true,
     },
     index: {
@@ -35,5 +47,21 @@ export default {
 <style scoped>
 .currency-pair {
   width: 100%;
+}
+button {
+  margin-top: 10px;
+}
+table {
+  width: 100%;
+  border-left: 2px solid #000;
+  border-bottom: 2px solid #000;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
+td {
+  width: 25%;
+  border-top: 2px solid #000;
+  border-right: 2px solid #000;
+  padding: 5px;
 }
 </style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class='home-container'>
+    <select v-model='currentPairLabel' class='currency-select'>
+      <option v-for='(label, index) in pairList' :key='index'>{{ label }}</option>
+    </select>
     <button @click.prevent='addPair'>+Добавить валютную пару</button>
+    <button @click.prevent='getData' class='refresh-button'>Обновить данные</button>
     <div class='currency-pairs'>
       <template v-for='(pair, index) in pairs'>
         <CurrencyPair
@@ -16,6 +20,7 @@
 
 <script>
 import CurrencyPair from '@/components/currency-pair';
+import axios from 'axios';
 
 export default {
   name: 'Home',
@@ -25,14 +30,42 @@ export default {
   data() {
     return {
       pairs: [],
+      currencyData: null,
+      currentPairLabel: null,
+      key: '',
     };
+  },
+  computed: {
+    market() {
+      return this.currencyData && this.currencyData.Markets && this.currencyData.Markets[0];
+    },
+    pairList() {
+      if (!this.market) { return []; }
+
+      const list = this.market.map(pair => pair.Label);
+
+      return list;
+    },
   },
   methods: {
     addPair() {
-      this.pairs.push('currency pair');
+      const pairLabel = this.currentPairLabel;
+
+      // eslint-disable-next-line
+      const currentPair = this.market.find((item) => item.Label === pairLabel);
+
+      this.pairs.push(currentPair);
     },
     deletePair(index) {
       this.pairs.splice(index, 1);
+    },
+    async getData() {
+      try {
+        const response = await axios.get(`https://www.worldcoinindex.com/apiservice/v2getmarkets?key=${this.key}&fiat=btc`);
+        this.currencyData = response.data;
+      } catch (error) {
+        throw new Error('Request error');
+      }
     },
   },
 };
@@ -44,5 +77,10 @@ export default {
   width: 800px;
   margin: 0 auto;
   text-align: left;
+}
+.currency-select {
+  min-width: 200px;
+}
+.refresh-button {
 }
 </style>

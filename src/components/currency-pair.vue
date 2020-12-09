a95970834da8523a7e7041a0778b0e92d38068d1
Radziyon Hryshayeu <rhryshayeu@allot.com>
2020-12-09 18:58:51 +0300

    Simple structure for home page.

---
+++ src/components/currency-pair.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class='currency-pair'>
+    {{ pair }}
+    {{ index }}
+    <button @click.prevent='deletePair'>Удалить</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CurrencyPair',
+  props: {
+    pair: {
+      type: String,
+      required: true,
+    },
+    index: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+    deletePair() {
+      this.$emit('delete', this.index);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.currency-pair {
+  width: 100%;
+}
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class='home-container'>
+    <button @click.prevent='addPair'>+Добавить валютную пару</button>
+    <div class='currency-pairs'>
+      <template v-for='(pair, index) in pairs'>
+        <CurrencyPair
+          :pair='pair'
+          :key='index'
+          :index='index'
+          @delete='deletePair'
+        />
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+import CurrencyPair from '@/components/currency-pair';
+
+export default {
+  name: 'Home',
+  components: {
+    CurrencyPair,
+  },
+  data() {
+    return {
+      pairs: [],
+    };
+  },
+  methods: {
+    addPair() {
+      this.pairs.push('currency pair');
+    },
+    deletePair(index) {
+      this.pairs.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home-container {
+  width: 800px;
+  margin: 0 auto;
+  text-align: left;
+}
+</style>
